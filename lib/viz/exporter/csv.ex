34651f5a0e5f481728261e3dab1d28cfbdcfd15f
Jonathan Arnett <jonarnett90@gmail.com>
2023-01-16 10:18:15 -0500

    Add CSV exporter

---
+++ lib/viz/exporter/csv.ex
@@ -0,0 +1,20 @@
+defmodule Viz.Exporter.CSV do
+  @moduledoc """
+  Exports call graphs as a CSV where the sources (callers) are in the first
+  column and their targets (callees) are in the second.
+  """
+
+  use Viz.Exporter
+
+  def export(mappings) do
+    csv_header = "source,target\n"
+
+    csv_body =
+      mappings
+      |> Enum.map(fn {s, t} -> ~s|"#{hash(s)}","#{hash(t)}"| end)
+      |> Enum.join("\n")
+
+    # Would an IO list work better here? 🤔
+    csv_header <> csv_body
+  end
+end
