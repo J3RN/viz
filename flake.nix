8da805e79740844a2426d8477f94da15585c7020
Jonathan Arnett <j3rn@j3rn.com>
2025-10-22 22:29:52 -0400

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Viz";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            pkgs.beamMinimal28Packages.elixir
+            pkgs.beamMinimal28Packages.elixir-ls
+          ];
+        };
+      }
+    );
+}
